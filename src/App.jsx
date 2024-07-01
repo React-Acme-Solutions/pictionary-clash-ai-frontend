0430869e6f70b2180c51e19acc57a19fce976291
Armen Agabekian <2008nv@gmail.com>
2024-07-01 13:08:20 -0700

    drawing

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@
 import React from "react";
 import FrontPage from "./components/Front/Front-Page";
 import "./styles/global-styles.scss";
+import DrawingCanvas from "./drawingCanvas";
 
 const App = () => {
   return (
     <div className="app">
-      <FrontPage />
+        <h1>TEAL KINGDOM</h1>
+      {/*<FrontPage />*/}
+        {/*integrate additional component here as needed*/}
+        {/*This is our entry point*/}
+        <DrawingCanvas/>
     </div>
   );
 };
