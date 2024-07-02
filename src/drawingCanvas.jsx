194b5ee100b040ad6115ee4f78aa195a18bb5e50
Jed Staley <JNStaley1@gmail.com>
2024-07-02 15:58:50 -0400

    working version

--- src/drawingCanvas.jsx
+++ src/drawingCanvas.jsx
@@ -10,26 +10,19 @@ const DrawingCanvas = () => {
 
   const handleCanvasChange = () => {
     const data = canvasRef.current.getSaveData();
-    test();
     draw(data);
-  };
-
-  const saveDrawing = () => {
-    const data = canvasRef.current.getSaveData();
-    console.log(data);
-  };
-
-  const loadDrawing = () => {
-    const data = "PASTE_SAVED_DATA_HERE"; // Replace with actual saved data
-    canvasRef.current.loadSaveData(data, true);
   };
 
   const clearDrawing = () => {
     canvasRef.current.clear();
+		const data = canvasRef.current.getSaveData();
+		draw(data);
   };
 
   const undoLast = () => {
     canvasRef.current.undo();
+		const data = canvasRef.current.getSaveData();
+		draw(data);
   };
 
   return (
@@ -44,10 +37,11 @@ const DrawingCanvas = () => {
         onChange={handleCanvasChange}
       />
       <div>
-        <button onClick={saveDrawing}>Save Drawing</button>
-        <button onClick={loadDrawing}>Load Drawing</button>
         <button onClick={clearDrawing}>Clear Drawing</button>
         <button onClick={undoLast}>Undo Last</button>
+				<button onClick={create}>Create</button>
+				<button onClick={join}>Join</button>
+				<button onClick={start}>Start</button>
       </div>
     </div>
   );
